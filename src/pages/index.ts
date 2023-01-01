eda2ecc1dc5977115627734843f9b27435859de3
light <estarosa@list.ru>
2023-01-01 21:44:26 +0300

    delete main page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
-import MainPage from './MainPage'
 import ProductsPage from './ProductsPage'
 import ProductPage from './ProductPage'
 import CartPage from './CartPage'
 import SearchProductsPage from './SearchProductPage'
 
-export { MainPage, ProductsPage, ProductPage, CartPage, SearchProductsPage, }
+export { ProductsPage, ProductPage, CartPage, SearchProductsPage, }
